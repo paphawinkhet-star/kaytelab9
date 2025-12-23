b9dcc96a2589aa66cb9f34d783c93968ee5a9b21
paphawinkhet-star <paphawin.khet@gmail.com>
2025-12-23 23:22:11 +0700

    complete lab9_1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,33 +1,50 @@
-#include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main() {
+    double loan, rate, payPerYear;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+    cout << "Enter initial loan: ";
+    cin >> loan;
+
+    cout << "Enter interest rate per year (%): ";
+    cin >> rate;
+
+    cout << "Enter amount you can pay per year: ";
+    cin >> payPerYear;
+
+    // ห้ามขึ้นบรรทัดใหม่
+    cout << setw(13) << left << "EndOfYear#"
+         << setw(13) << left << "PrevBalance"
+         << setw(13) << left << "Interest"
+         << setw(13) << left << "Total"
+         << setw(13) << left << "Payment"
+         << setw(13) << left << "NewBalance"
+         << "\n";
+
+    cout << fixed << setprecision(2);
+
+    int year = 1;
+    double prevBalance = loan;
+
+    while (prevBalance > 0) {
+        double interest = prevBalance * rate / 100.0;
+        double total = prevBalance + interest;
+        double payment = (total <= payPerYear) ? total : payPerYear;
+        double newBalance = total - payment;
+
+        cout << setw(13) << left << year
+             << setw(13) << left << prevBalance
+             << setw(13) << left << interest
+             << setw(13) << left << total
+             << setw(13) << left << payment
+             << setw(13) << left << newBalance
+             << "\n";
+
+        prevBalance = newBalance;
+        year++;
+    }
+
+    return 0;
 }
